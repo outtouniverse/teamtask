c5545b6bec6f7f36fdbb5249cac9e07f18467ffe
outtouniverse <aakub1096@gmail.com>
2024-12-25 20:51:16 +0530

    view_err

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ const app = express();
 // MongoDB connection
 const mongoURI = 'mongodb://localhost:27017/todo';
 mongoose.connect(mongoURI);
-
+app.set('views', path.join(__dirname, 'views')); 
 app.set('view engine', 'ejs');
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
